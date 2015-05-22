32e8d156fb4748f79b2189fcd4f14faf46e9951f
Oleg Sofrygin <olegso@gmail.com>
2015-05-21 18:11:41 -0700

    test

--- tests/RUnit/runittests.R
+++ tests/RUnit/runittests.R
@@ -20,14 +20,12 @@ if(FALSE) {
   # devtools::build_win() # build package on CRAN servers:
   # check R package tar ball:
   # setwd("..");
-  # system("R CMD check simcausal_0.1.tar.gz") 
+  # system("R CMD check simcausal_0.1.tar.gz")
   # check R package tar ball prior to CRAN submission:
-  # system("R CMD check --as-cran simcausal_0.1.tar.gz") 
-
+  # system("R CMD check --as-cran simcausal_0.1.tar.gz")
       ## system("R CMD check --no-manual --no-vignettes simcausal") # check without building the pdf manual and not building vignettes
       ## system("R CMD build simcausal --no-build-vignettes")
       ## system("R CMD build simcausal")
-
   # SET UP TRAVIS CONFIG FILE:
   # devtools::use_travis()
 
