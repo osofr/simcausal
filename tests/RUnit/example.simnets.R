4bb920bbe4976917d21e2624589ccc2d55aa7885
Oleg Sofrygin <olegso@gmail.com>
2016-01-26 17:51:29 -0800

    simplifying examples

--- tests/RUnit/example.simnets.R
+++ tests/RUnit/example.simnets.R
@@ -22,23 +22,16 @@ gen.ER <- function(n, m_pn, ...) {
 }
 
 D <- DAG.empty()
-
 # Sample ER model network using igraph::sample_gnm with m_pn argument:
-D <- D + network("Net1", netfun = "gen.ER", m_pn = 50)
-
-# W1 - categorical (5 categories, 0-4):
-nW1cat <- 6
-rbinom2 <- function(n, size, prob) rbinom(n, size = size, prob = prob[1,])
-D <- D + node("W1", distr = "rbinom2", size = (nW1cat-1), prob = c(0.4, 0.5, 0.7, 0.4))
-
+D <- D + network("ER.net", netfun = "gen.ER", m_pn = 50)
+# W1 - categorical (6 categories, 1-6):
+D <- D +
+  node("W1", distr = "rcategor.int",
+        probs = c(0.0494, 0.1823, 0.2806, 0.2680, 0.1651, 0.0546)) +
 # W2 - binary infection status at t=0, positively correlated with W1:
-prob_W2 <- seq.int(0.45, 0.8, length.out = nW1cat)
-D <- D + node("W2", distr = "rbern", asis.params = list(prob = "prob_W2[W1+1]"))
-
+  node("W2", distr = "rbern", prob = plogis(-0.2 + W1/3)) +
 # W3 - binary confounder:
-prob_W3 <- 0.6
-D <- D + node("W3", distr = "rbern", prob = prob_W3)
-
+  node("W3", distr = "rbern", prob = 0.6)
 # A[i] is a function W1[i] and the total of i's friends values W1, W2 and W3:
 D <- D + node("A", distr = "rbern",
               prob = plogis(2 + -0.5 * W1 +
@@ -46,7 +39,6 @@ D <- D + node("A", distr = "rbern",
                             -0.4 * sum(W2[[1:Kmax]]) +
                             -0.7 * sum(W3[[1:Kmax]])),
               replaceNAw0 = TRUE)
-
 # Y[i] is a function of netW3 (friends of i W3 values) and the total N of i's friends 
 # who are infected AND untreated:
 D <- D + node("Y", distr = "rbern",
@@ -54,7 +46,6 @@ D <- D + node("Y", distr = "rbern",
                             -2 * sum(W3[[1:Kmax]])
                             ),
               replaceNAw0 = TRUE)
-
 # Can add N untreated friends to the above outcome Y equation: sum(1 - A[[1:Kmax]]):
 D <- D + node("Y", distr = "rbern",
               prob = plogis(-1 + 1.5 * sum(W2[[1:Kmax]] * (1 - A[[1:Kmax]])) +
@@ -62,7 +53,6 @@ D <- D + node("Y", distr = "rbern",
                             0.25 * sum(1 - A[[1:Kmax]])
                             ),
               replaceNAw0 = TRUE)
-
 # Can add N infected friends at baseline to the above outcome Y equation: sum(W2[[1:Kmax]]):
 D <- D + node("Y", distr = "rbern",
               prob = plogis(-1 + 1 * sum(W2[[1:Kmax]] * (1 - A[[1:Kmax]])) +
@@ -71,13 +61,10 @@ D <- D + node("Y", distr = "rbern",
                             0.25 * sum(W2[[1:Kmax]])
                             ),
               replaceNAw0 = TRUE)
-
 Dset <- set.DAG(D)
-
 # Simulating data from the above sem:
 datnet <- sim(Dset, n = 1000, rndseed = 543)
 head(datnet, 100)
-
 # Obtaining the network object from simulated data:
 net_object <- attributes(datnet)$netind_cl
 # Max number of friends:
@@ -92,77 +79,61 @@ head(attributes(datnet)$netind_cl$NetInd)
 #--------------------------------------------------------------------------------------------------
 # Example of a user-defined network sampler(s) function
 # Arguments K, bslVar[i] (W1) & nF are evaluated in the environment of the simulated data then 
-# passed to generateNET() function
-  # - unif.F: when TRUE sample friends set as discrete uniform distr (no weighting by bslVar)
+# passed to genNET() function
+  # - K: maximum number of friends for any unit
   # - bslVar[i]: used for contructing weights for the probability of selecting i as 
   # someone else's friend (weighted sampling), when missing the sampling goes to uniform
   # - nF[i]: total number of friends that need to be sampled for observation i
 #--------------------------------------------------------------------------------------------------
-generateNET <- function(n, K, unif.F = FALSE, bslVar, nF, ...) {
-  nW1cat <- 6
-  W1cat_arr <- c(1:nW1cat)/2
-  prob_F <- plogis(-4.5 + 2.5*W1cat_arr) / sum(plogis(-4.5 + 2.5*W1cat_arr))
+genNET <- function(n, K, bslVar, nF, ...) {
+  print("K: "); print(K)
+  prob_F <- plogis(-4.5 + 2.5*c(1:K)/2) / sum(plogis(-4.5 + 2.5*c(1:K)/2))
+  print("prob_F"); print(prob_F)
+
   NetInd_k <- matrix(NA_integer_, nrow = n, ncol = K)
   nFriendTot <- rep(0L, n)
-
   for (index in (1:n)) {
-    FriendSampSet <- setdiff( c(1:n), index)
+    FriendSampSet <- setdiff(c(1:n), index)
     nFriendSamp <- max(nF[index] - nFriendTot[index], 0L)
-    if (nFriendSamp>0) {
-      if (length(FriendSampSet)==1)  {
+    if (nFriendSamp > 0) {
+      if (length(FriendSampSet) == 1)  {
         friends_i <- FriendSampSet
       } else {
-        if (missing(bslVar) || unif.F[1]) {
-          friends_i <- sort(sample(FriendSampSet, size = nFriendSamp))
-        } else {
-          friends_i <- sort(sample(FriendSampSet, size = nFriendSamp,
-                            prob = prob_F[bslVar[FriendSampSet] + 1]))
-        }
+        friends_i <- sort(sample(FriendSampSet, size = nFriendSamp,
+                          prob = prob_F[bslVar[FriendSampSet] + 1]))
       }
-      NetInd_k[index, ] <- c(as.integer(friends_i), rep_len(NA_integer_, K - length(friends_i)))
+      NetInd_k[index, ] <- c(as.integer(friends_i),
+                            rep_len(NA_integer_, K - length(friends_i)))
       nFriendTot[index] <- nFriendTot[index] + nFriendSamp
     }
   }
   return(NetInd_k)
 }
 
-K <- 6
+rcategor.int.base0 <- function(n, probs) rcategor.int(n, probs)-1
 D <- DAG.empty()
-
+D <- D +
 # W1 - categorical or continuous confounder (5 categories, 0-4):
-nW1cat <- 6
-rbinom2 <- function(n, size, prob) rbinom(n, size = size, prob = prob[1,])
-D <- D + node("W1", distr = "rbinom2", size = (nW1cat-1), prob = c(0.4, 0.5, 0.7, 0.4))
-
+  node("W1", distr = "rcategor.int.base0",
+        probs = c(0.0494, 0.1823, 0.2806, 0.2680, 0.1651, 0.0546)) +
 # W2 - binary infection status at t=0, positively correlated with W1:
-prob_W2 <- seq.int(0.45, 0.8, length.out = nW1cat)
-D <- D + node("W2", distr = "rbern",
-            prob = (W1==0)*.(prob_W2[1]) + (W1==1)*.(prob_W2[2]) + (W1==2)*.(prob_W2[3]) +
-                   (W1==3)*.(prob_W2[4]) + (W1==4)*.(prob_W2[5]) + (W1==5)*.(prob_W2[6]))
-
+  node("W2", distr = "rbern", prob = plogis(-0.2 + W1/3)) +
 # W3 - binary confounder:
-prob_W3 <- 0.6
-D <- D + node("W3", distr = "rbern", prob = prob_W3)
+  node("W3", distr = "rbern", prob = 0.6)
 
-# nF: total number of friends for each i (nF[i]), each nF[i] is influenced by categorical W1 
+# def.nF: total number of friends for each i (0-K), each def.nF[i] is influenced by categorical W1 
+K <- 10
+set.seed(12345)
 normprob <- function(x) x / sum(x)
-k_arr <-c(1:K)
-pN_0 <- 0.02
-prob_Ni_W1_0 <- normprob(c(pN_0, plogis(-3 - 0 - k_arr / 2)))    # W1=0 probabilities of |F_i|
-prob_Ni_W1_1 <- normprob(c(pN_0, plogis(-1.5 - 0 - k_arr / 3)))  # W1=1 probabilities of |F_i|
-prob_Ni_W1_2 <- normprob(c(pN_0, pnorm(-2*abs(2 - k_arr) / 5)))  # W1=2 probabilities of |F_i|
-prob_Ni_W1_3 <- normprob(c(pN_0, pnorm(-2*abs(3 - k_arr) / 5)))  # W1=3 probabilities of |F_i|
-prob_Ni_W1_4 <- normprob(c(pN_0, plogis(-4 + 2 * (k_arr - 2))))  # W1=4 probabilities of |F_i|
-prob_Ni_W1_5 <- normprob(c(pN_0, plogis(-4 + 2 * (k_arr - 3))))  # W1=5 probabilities of |F_i|
-
-D <- D + node("nF.plus1", distr = "rcategor.int",
-              probs = (W1 == 0)*.(prob_Ni_W1_0) + (W1 == 1)*.(prob_Ni_W1_1) +
-                      (W1 == 2)*.(prob_Ni_W1_2) + (W1 == 3)*.(prob_Ni_W1_3) +
-                      (W1 == 4)*.(prob_Ni_W1_4) + (W1 == 5)*.(prob_Ni_W1_5))
+p_nF_W1_mat <- apply(matrix(runif((K+1)*6), ncol = 6, nrow = (K+1)), 2, normprob)
+colnames(p_nF_W1_mat) <- paste0("p_nF_W1_", c(0:5))
+colSums(p_nF_W1_mat)
+create_probs_nF <- function(W1) t(p_nF_W1_mat[,W1+1])
+vecfun.add("create_probs_nF")
+D <- D + node("def.nF", distr = "rcategor.int.base0", probs = create_probs_nF(W1))
 
 # Adding the network generator that depends on nF and categorical W1:
-D <- D + network("NetInd_k", K = K, netfun = "generateNET", bslVar = W1, nF = nF.plus1 - 1)
-
+D <- D + network(name="net.custom", netfun = "genNET", K = K, bslVar = W1, nF = def.nF)
 # Define A[i] is a function W1[i] as well as the total sum of i's friends values for W1, W2 and W3:
 D <- D + node("A", distr = "rbern",
               prob = plogis(2 + -0.5 * W1 +
@@ -170,7 +141,6 @@ D <- D + node("A", distr = "rbern",
                             -0.4 * sum(W2[[1:Kmax]]) +
                             -0.7 * sum(W3[[1:Kmax]])),
               replaceNAw0 = TRUE)
-
 # Y[i] is a the total N of i's friends who are infected AND untreated
 # + a function of friends W3 values
 D <- D + node("pYRisk", distr = "rconst",
@@ -183,3 +153,10 @@ Dset <- set.DAG(D)
 
 # Simulating data from the above sem:
 datnet <- sim(Dset, n = 1000, rndseed = 543)
+head(datnet, 100)
+# Obtaining the network object from simulated data:
+net_object <- attributes(datnet)$netind_cl
+# Max number of friends:
+net_object$Kmax
+# Network matrix
+head(attributes(datnet)$netind_cl$NetInd)
