a6bc1c0f7436416f70c63492fce322bcafbc6b9d
Oleg Sofrygin <olegso@gmail.com>
2016-02-16 08:58:43 -0800

    switching to production version 0.5.0

--- tests/RUnit/RUnit_tests_00.R
+++ tests/RUnit/RUnit_tests_00.R
@@ -57,47 +57,61 @@ if(FALSE) {
 psi_RDs_DAG2a <- NULL
 psi_RDs_DAG2b <- NULL
 
-sample_checks <- function() {   # doesn't run, this is just to show what test functions can be used
+sample_checks <- function() {   # doesnt run, this is just to show what test functions can be used
   print("Starting tests...")
-    checkTrue(1 < 2, "check1")     ## passes fine
-     ## checkTrue(1 > 2, "check2")  ## appears as failure in the test protocol
-     v <- 1:3
-     w <- 1:3
-     checkEquals(v, w)               ## passes fine
-     names(v) <- c("A", "B", "C")
-     ## checkEquals(v, w)            ## fails because v and w have different names
-     checkEqualsNumeric(v, w)        ## passes fine because names are ignored
-     x <- rep(1:12, 2)
-     y <- rep(0:1, 12)
-     res <- list(a=1:3, b=letters, LM=lm(y ~ x))
-     res2 <- list(a=seq(1,3,by=1), b=letters, LM=lm(y ~ x))
-     checkEquals( res, res2)        ## passes fine
-     checkIdentical( res, res)
-     checkIdentical( res2, res2)
-     ## checkIdentical( res, res2)  ## fails because element 'a' differs in type
-     fun <- function(x) {
-       if(x)
-       {
-        stop("stop conditions signaled")
-       }
-       return()
-     }
-     checkException(fun(TRUE))      ## passes fine
-     ## checkException(fun(FALSE))  ## failure, because fun raises no error
-     checkException(fun(TRUE), silent=TRUE)
-     ##  special constants
-     ##  same behaviour as for underlying base functions
-     checkEquals(NA, NA)
-     checkEquals(NaN, NaN)
-     checkEquals(Inf, Inf)
-     checkIdentical(NA, NA)
-     checkIdentical(NaN, NaN)
-     checkIdentical(-Inf, -Inf)
+  checkTrue(1 < 2, "check1")     ## passes fine
+  ## checkTrue(1 > 2, "check2")  ## appears as failure in the test protocol
+  v <- 1:3
+  w <- 1:3
+  checkEquals(v, w)               ## passes fine
+  names(v) <- c("A", "B", "C")
+  ## checkEquals(v, w)            ## fails because v and w have different names
+  checkEqualsNumeric(v, w)        ## passes fine because names are ignored
+  x <- rep(1:12, 2)
+  y <- rep(0:1, 12)
+  res <- list(a=1:3, b=letters, LM=lm(y ~ x))
+  res2 <- list(a=seq(1,3,by=1), b=letters, LM=lm(y ~ x))
+  checkEquals( res, res2)        ## passes fine
+  checkIdentical( res, res)
+  checkIdentical( res2, res2)
+  ## checkIdentical( res, res2)  ## fails because element 'a' differs in type
+  fun <- function(x) {
+   if(x)
+   {
+    stop("stop conditions signaled")
+   }
+   return()
+  }
+  checkException(fun(TRUE))      ## passes fine
+  ## checkException(fun(FALSE))  ## failure, because fun raises no error
+  checkException(fun(TRUE), silent=TRUE)
+  ##  special constants
+  ##  same behaviour as for underlying base functions
+  checkEquals(NA, NA)
+  checkEquals(NaN, NaN)
+  checkEquals(Inf, Inf)
+  checkIdentical(NA, NA)
+  checkIdentical(NaN, NaN)
+  checkIdentical(-Inf, -Inf)
 }
 
 `%+%` <- function(a, b) paste0(a, b)
 as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 allNA = function(x) all(is.na(x))
+
+
+test.Nsamp.n.test <- function() {
+  D <- DAG.empty()
+  D <- D + node("A", distr = "rbern", prob=0.5) + 
+    node("N", distr = "rconst", const=Nsamp)
+  Dset <- set.DAG(D)
+  sim(Dset, n = 100)
+
+  Dset.ntest <- set.DAG(D, n.test = 200)
+
+  Dset.fail <- set.DAG(D, n.test = 0L)
+}
+
 
 # Adding test for latent vars
 test.latent <- function() {
