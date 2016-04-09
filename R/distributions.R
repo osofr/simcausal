b493bfdae9d5bf0bbfcad9fbfcf04e9e6e98dc4e
Oleg Sofrygin <olegso@gmail.com>
2016-04-09 12:10:11 -0700

    adding two network generators based on Erdos-Renyi model; adding base0 cat distr (rcat.b0) and changing the names of categorical distributions to be more consistent; rcategor -> rcat.factor; rcategor.int -> rcat.b1

--- R/distributions.R
+++ R/distributions.R
@@ -1,5 +1,5 @@
 
-#' Bernoulli Node Distribution
+#' Random Sample from Bernoulli Distribution
 #'
 #' Wrapper for Bernoulli node distribution.
 #'
@@ -22,7 +22,7 @@ rbern <- function(n, prob) {
 	rbinom(n=n, prob=prob, size=1)
 }
 
-#' Constant (Degenerate) Node Distribution
+#' Constant (Degenerate) Distribution (Returns its Own Argument \code{const})
 #'
 #' Wrapper for constant value (degenerate) distribution.
 #'
@@ -64,12 +64,18 @@ rconst <- function(n, const) {
 	}
 }
 
-#' Categorical Node Distribution (Factor)
+#' Random Sample for a Categorical Factor
 #'
-#' Matrix version of the categorical distribution. The argument \code{probs} can be a matrix of n rows, specifying individual (varying in sample) categorical probabilities. The number of categories generated is equal to \code{ncol(probs)+1}, the categories are labeled as: \code{1,...,ncol(probs)+1}.
+#' Matrix version of the categorical distribution. The argument \code{probs} can be a matrix of n rows,
+#' specifying individual (varying in sample) categorical probabilities.
+#' The number of categories generated is equal to \code{ncol(probs)+1}, the levels labeled as: \code{1,...,ncol(probs)+1}.
 #'
 #' @param n Sample size.
-#' @param probs Either a vector or a matrix of success probabilities. When \code{probs} is a vector, \code{n} identically distributed random categorical variables are generated with categories: 1, 2, ..., length(probs)+1. When \code{probs} is a matrix, the categorical probabilities of the \code{k}th sample are determined by the \code{k}th row of \code{probs} matrix, i.e., \code{probs[k,]}.
+#' @param probs Either a vector or a matrix of success probabilities.
+#' When \code{probs} is a vector, \code{n} identically distributed random categorical variables
+#' are generated with categories: 1, 2, ..., length(probs)+1.
+#' When \code{probs} is a matrix, the categorical probabilities of the \code{k}th sample are determined by the
+#' \code{k}th row of \code{probs} matrix, i.e., \code{probs[k,]}.
 #' @return A factor of length \code{n} with levels: \code{1,2, ...,ncol(probs)+1}.
 #' @examples
 #'
@@ -78,7 +84,7 @@ rconst <- function(n, const) {
 #'# probabilities
 #'#---------------------------------------------------------------------------------------
 #'D <- DAG.empty()
-#'D <- D + node("race",t=0,distr="rcategor",probs=c(0.2,0.1,0.4,0.15,0.05,0.1))
+#'D <- D + node("race",t=0,distr="rcat.factor",probs=c(0.2,0.1,0.4,0.15,0.05,0.1))
 #'Dset <- set.DAG(D)
 #'simdat <- sim(Dset, n=200, rndseed=1)
 #'
@@ -89,47 +95,46 @@ rconst <- function(n, const) {
 #'D <- DAG.empty()
 #'D <- D + node("L0", distr="rnorm", mean=10, sd=5)
 #'D <- D + node("L1", distr="rnorm", mean=10, sd=5)
-#'D <- D + node("L2", distr="rcategor", probs=c(abs(1/L0), abs(1/L1)))
+#'D <- D + node("L2", distr="rcat.factor", probs=c(abs(1/L0), abs(1/L1)))
 #'Dset <- set.DAG(D)
 #'simdat <- sim(Dset, n=200, rndseed=1)
+#' @seealso \code{\link{rcat.b1}}, \code{\link{rcat.b0}}
 #' @export
-rcategor <- function(n, probs) {
-	# if (is.vector(probs)) {
-	# 	probs <- matrix(data = probs, nrow = n, ncol = length(probs), byrow = TRUE)
-	# }
-	# probs <- cbind(probs, 1 - rowSums(probs))	# sum each row and check if some need to be normalized
-	# pover1_idx <- which(probs[,ncol(probs)] < -(10^-6)) # which rows of probs need to be normalized
-	# # pover1_idx <- which(probs[,ncol(probs)] < 0) # which rows of probs need to be normalized
-
-	# # reset last category to zero if its with numeric epsilon error away from 0:
-	# restto0 <- which(abs(probs[,ncol(probs)]) <= 10^-6 & abs(probs[,ncol(probs)]) > 0L)
-	# if (length(restto0)>0) {
-	# 	probs[restto0, ncol(probs)] <- 0L
-	# }
-
-	# if (length(pover1_idx)>0) {
-	# 	warning("some categorical probabilities add up to more than 1, normalizing to add to 1")
-	# 	probs[pover1_idx, ncol(probs)] <- 0
-	# 	probs[pover1_idx, ] <- probs[pover1_idx, ,drop = FALSE] / rowSums(probs[pover1_idx, ,drop = FALSE]) # normalize
-	# }
-	# probs_cum <- matrix(nrow=nrow(probs), ncol=ncol(probs))
-	# probs_cum[,1] <- probs[,1]
-	# for (i in seq(ncol(probs))[-1]) {
-	# 	probs_cum[,i] <- rowSums(probs[,c(1:i)])
-	# }
-	# samples <- rowSums(probs_cum - runif(nrow(probs_cum)) < 0) + 1
-	# as.factor(samples)
+rcat.factor <- function(n, probs) {
 	as.factor(rcategor.int(n = n, probs = probs))
 }
 
-#' Categorical Node Distribution (Integer)
+#' @describeIn rcat.factor (Deperecated) Random Sample of a Categorical Factor
+#' @export
+rcategor <- function(n, probs) {
+	warning("This function is deprecated, please use rcat.factor() instead.")
+	as.factor(rcategor.int(n = n, probs = probs))
+}
+
+#' Random Sample from Base 1 (rcat.b1) or Base 0 (rcat.b0) Categorical (Integer) Distribution
 #'
-#' Same as \code{rcategor}, but returning a vector of sampled integers 1, 2, ..., \code{ncol(probs)+1} instead of a factor.
+#' Same as \code{}, but returning a vector of sampled integers with range 1, 2, ..., \code{ncol(probs)+1} for \code{rcat.b1}
+#' or range 0, 1, ..., \code{ncol(probs)} for \code{rcat.b0}. For sampling categorical factors see \link{rcat.factor}.
 #' @param n Sample size.
-#' @param probs Either a vector or a matrix of success probabilities. When probs is a vector, \code{n} identically distributed random categorical variables are generated with categories: 1, 2, ..., \code{length(probs)+1}. When \code{probs} is a matrix, the categorical probabilities of the \code{k}th sample are determined by the \code{k}th row of probs matrix, i.e., \code{probs[k,]}.
-#' @return An integer vector of length \code{n} with values: \code{1,2, ...,ncol(probs)+1}.
+#' @param probs Either a vector or a matrix of success probabilities.
+#' When probs is a vector, \code{n} identically distributed random categorical variables are
+#' generated.
+#' When \code{probs} is a matrix, the categorical probabilities of the \code{k}th
+#' sample are determined by the \code{k}th row of probs matrix, i.e., \code{probs[k,]}.
+#' @return An integer vector of length \code{n} with range either in \code{0,...,ncol(probs)} or in \code{1,...,ncol(probs)+1}.
+#' @seealso \code{\link{rcat.factor}}
+#' @describeIn rcat.b1 Random Sample from Base 1 Categorical (Integer) Distribution
+#' @export
+rcat.b1 <- function(n, probs) rcategor.int(n, probs)
+
+#' @describeIn rcat.b1 Random Sample from Base 0 Categorical (Integer) Distribution
+#' @export
+rcat.b0 <- function(n, probs) rcat(n, probs)-1
+
+#' @describeIn rcat.b1 (Deperecated) Random Sample from Base 1 Categorical (Integer) Distribution
 #' @export
 rcategor.int <- function(n, probs) {
+	warning("This function is deprecated, please use rcat.b1() instead.")
 	if (n==0) {
 		probs <- matrix(nrow = n, ncol = length(probs), byrow = TRUE)
 	}
@@ -167,7 +172,8 @@ rcategor.int <- function(n, probs) {
 #' @export
 distr.list <- function() {
 	message("All custom distributions defined in SimCausal:\n")
-	print(ls("package:simcausal", pattern="^[r]"))
+	print(ls("package:simcausal")[grep(pattern = "^r(?!net)", x = ls("package:simcausal"), perl=TRUE)])
+	# print(ls("package:simcausal", pattern="^[r]"))
 	# print(ls("package:simcausal", pattern="^[S]L"))
 	invisible(ls("package:simcausal"))
 }
@@ -176,10 +182,15 @@ distr.list <- function() {
 #'
 #' Template function for writing \code{SimCausal} custom distribution wrappers.
 #'
-#' One of the named arguments must be 'n', this argument is passed on to the function automatically by the package and is assigned to the number of samples that needs to be generated from this distribution.
-#' Other arguments (in this example arg1 and arg2) must be declared by the user as arguments inside the node() function that uses this distribution,
+#' One of the named arguments must be 'n', this argument is passed on to the function automatically by
+#' the package and is assigned to the number of samples that needs to be generated from this distribution.
+#' Other arguments (in this example arg1 and arg2) must be declared by the user as
+#' arguments inside the node() function that uses this distribution,
 #' e.g., \code{node("Node1"}, \code{distr="distr.template"}, \code{arg1 = ...}, \code{arg2 = ...)}.
-#' Both, arg1 and arg2, can be either numeric constants or formulas involving past node names. The constants get passed on to the distribution function unchanged. The formulas are evaluated inside the environment of the simulated data and are passed on to the distribution functions as vectors.
+#' Both, arg1 and arg2, can be either numeric constants or formulas involving past node names.
+#' The constants get passed on to the distribution function unchanged.
+#' The formulas are evaluated inside the environment of the simulated data and are passed on to the
+#' distribution functions as vectors.
 #' The output of the distribution function is expected to be a vector of length n of the sampled covariates.
 #'
 #' @param n Sample size that needs to be generated
---
+++ R/network_distributions.R
@@ -0,0 +1,42 @@
+#' Call \code{igraph::sample_gnp} to generate random graph object according to the G(n,p) Erdos-Renyi model
+#'
+#' Call \code{igraph::sample_gnp} and convert the output to \code{simcausal} network matrix.
+#' @param n Size of the network graph (number of nodes).
+#' @param p Same as \code{igraph::sample_gnp}: The probability for drawing an edge between two arbitrary vertices (G(n,p) graph).
+#' @return A matrix with n rows, each row lists the indices of friends connected to that particular observation.
+#' @seealso \code{\link{rnet.gnm}}
+#' @export
+rnet.gnp <- function(n, p,  ...) {
+  igraph.gnm <- igraph::sample_gnp(n = n, p = p, directed = TRUE)
+  sparse_AdjMat <- simcausal::igraph.to.sparseAdjMat(igraph.gnm)
+  NetInd_out <- simcausal::sparseAdjMat.to.NetInd(sparse_AdjMat)
+  return(NetInd_out$NetInd_k)
+}
+
+#' Call \code{igraph::sample_gnm} to generate random graph object according to the G(n,m) Erdos-Renyi model
+#'
+#' Call \code{igraph::sample_gnm} and convert the output to \code{simcausal} network matrix.
+#' The parameter \code{m} of \code{igraph::sample_gnm} is derived from \code{n} and \code{m_pn} as \code{as.integer(m_pn*n)}
+#' @param n Size of the network graph (number of nodes).
+#' @param m_pn The total number of edges as a fraction of the sample size \code{n}.
+#'
+#' @return A matrix with n rows, each row lists the indices of friends connected to that particular observation.
+#' @seealso \code{\link{rnet.gnp}}
+#' @export
+rnet.gnm <- function(n, m_pn,  ...) {
+  m <- as.integer(m_pn*n)
+  if (n <= 10) m <- 20
+  igraph.gnm <- igraph::sample_gnm(n = n, m = m, directed = TRUE)
+  sparse_AdjMat <- simcausal::igraph.to.sparseAdjMat(igraph.gnm)
+  NetInd_out <- simcausal::sparseAdjMat.to.NetInd(sparse_AdjMat)
+  return(NetInd_out$NetInd_k)
+}
+
+#' List All Custom Network Generator Functions in \code{simcausal}.
+#'
+#' @export
+net.list <- function() {
+  message("All custom network generators defined in SimCausal:\n")
+  print(ls("package:simcausal", pattern="^(rnet)"))
+  invisible(ls("package:simcausal"))
+}
